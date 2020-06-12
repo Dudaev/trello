7b2d5cfe92c47d9401c99165cd9f57df24bce77c
Your Name <you@example.com>
2020-06-13 00:42:02 +0600

    Add  Board.jsx

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Lists from "./Lists/Lists";
+import Popup from "./Popup/Popup";
+
+const Board = () => {
+    return (
+        <div>
+            <Lists/>
+            <Popup/>
+        </div>
+    );
+};
+
+export default Board;
